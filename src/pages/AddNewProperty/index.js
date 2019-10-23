cf26d40000ef55ab8c56b5b98242f2767f03f33b
Yordan Galabov <galabovj@gmail.com>
2019-10-23 07:30:26 +0300

    add form fields imageComponent and format data for database

--- src/pages/AddNewProperty/index.js
+++ src/pages/AddNewProperty/index.js
@@ -1,5 +1,5 @@
-import React, { useState, Fragment } from "react";
-import { withFormik, FieldArray } from "formik";
+import React, { Fragment, useRef } from "react";
+import { withFormik } from "formik";
 import * as Yup from "yup";
 import {
   FormControl,
@@ -15,7 +15,10 @@ import {
   InputAdornment,
   Checkbox,
   FormControlLabel,
-  FormLabel
+  FormLabel,
+  Grid,
+  Tooltip,
+  Fab
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import {
@@ -24,10 +27,11 @@ import {
   cities,
   features
 } from "./form-helpers";
+import AddIcon from "@material-ui/icons/Add";
 
 const PropertyFormSchema = Yup.object().shape({
   //   propertySellType: Yup.string().required("Required"),
-  address: Yup.string().required("Required")
+  // address: Yup.string().required("Required")
   //   lastName: Yup.string()
   //     .min(2, "Too Short!")
   //     .max(50, "Too Long!")
@@ -36,30 +40,6 @@ const PropertyFormSchema = Yup.object().shape({
   //     .email("Invalid email")
   //     .required("Required")
 });
-
-// real_estates_id  -- NOT SHOWING IN FRONT END ONLY SUBMITING
-// real_estates_title --- STRING - generate from  selltype, construction-type, neighboorhood
-// real_estates_sell_type -- SELECT
-// real_estates_construction_type -- SELECT -- 2-STAEN ETC
-// real_estates_neighborhood --- SELECT
-
-// real_estates_city --- SELECT
-// real_estates_address -- STRING  -> done
-
-// real_estates_original_price --- ONLY EURO TEXT FIELD WITH NUMBER
-// real_estates_price_in_euro -- AUTO- CALCULATE
-// real_estates_currency  ---> default euro
-// real_estates_price_per_square  --> auto calculate
-// real_estates_price_per_square_in_euro ---> same as above
-// real_estates_size --> done
-// real_estates_floor
-// real_estates_tec
-// real_estates_phone
-// real_estates_description
-// real_estates_imageNames
-// real_estates_seller_phone_number
-// real_estates_seller_features
-// real_estates_created_by
 
 const useStyles = makeStyles(theme => ({
   mainContainer: {
@@ -84,7 +64,7 @@ const useStyles = makeStyles(theme => ({
     flexDirection: "column",
     [theme.breakpoints.up("sm")]: {
       flexDirection: "row",
-      justifyContent: "space-around"
+      justifyContent: "space-between"
     }
   },
   featuresContainer: {
@@ -98,6 +78,9 @@ const useStyles = makeStyles(theme => ({
   },
   selectContainer: {
     minWidth: 200
+  },
+  textField: {
+    maxWidth: 200
   },
   description: {
     flexBasis: "100%"
@@ -158,6 +141,125 @@ const FormTextField = ({
   </div>
 );
 
+function AddImageComponent({ values, index, classes, onDrop }) {
+  const inputRef = useRef(null);
+  const handleChange = (e, index) => {
+    // get the files
+    let file = e.target.files[0];
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      // Make a fileInfo Object
+      let fileInfo = {
+        name: file.name,
+        type: file.type,
+        size: Math.round(file.size / 1000) + " kB",
+        base64: reader.result
+      };
+      onDrop(fileInfo, index);
+    };
+  };
+
+  const handleClick = () => {
+    inputRef.current.click();
+  };
+
+  // eslint-disable-next-line no-eval
+  if (eval(`values.images.image${index + 1}`)) {
+    const image = eval(`values.images.image${index + 1}`);
+    return (
+      <div
+        className={classes.imageContainer}
+        key={`image.name-${index}`}
+        cols={1}
+      >
+        <img src={image.base64} alt={image.name} />
+      </div>
+    );
+  } else {
+    return (
+      <div className={classes.noImage} cols={1} onClick={handleClick}>
+        <input
+          ref={inputRef}
+          onChange={e => handleChange(e, index)}
+          type="file"
+        ></input>
+        <Tooltip title="Add" aria-label="add">
+          <Fab color="primary" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+        </Tooltip>
+      </div>
+    );
+  }
+}
+
+const useImageStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    marginTop: theme.spacing(2)
+  },
+  imageContainer: {
+    "& img": {
+      width: "100%",
+      objectFit: "contain"
+    }
+  },
+  noImage: {
+    display: "flex",
+    justifyContent: "center",
+    "& input": {
+      display: "none",
+      zIndex: 100,
+      width: "100%",
+      height: "100%"
+    }
+  }
+}));
+
+export function ImageGridList({ key, values, onDrop, imagesLength }) {
+  const classes = useImageStyles();
+  const imagesArray = [...Array(imagesLength)];
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h4">
+        <Box textAlign="center">Добавяне на снимки</Box>
+      </Typography>
+      <Grid
+        className={classes.gridConainer}
+        container
+        spacing={2}
+        justify="center"
+      >
+        {imagesArray.map((_, index) => {
+          return (
+            <Grid
+              item
+              key={`image-${index}`}
+              xs={4}
+              md={3}
+              lg={2}
+              className={classes.imageComponentContainer}
+            >
+              <AddImageComponent
+                classes={classes}
+                values={values}
+                onDrop={onDrop}
+                index={index}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
+}
+
 const Form = ({
   values,
   handleBlur,
@@ -169,6 +271,9 @@ const Form = ({
   errors
 }) => {
   const classes = useStyles();
+  const onDrop = (picture, index) => {
+    setFieldValue(`images.image${index + 1}`, picture);
+  };
   return (
     <Container className={classes.mainContainer} maxWidth="md">
       <Typography className={classes.title} variant="h4">
@@ -259,6 +364,16 @@ const Form = ({
         <section className={classes.sectionContainer}>
           <FormTextField
             className={classes.textField}
+            label="Телефонен номер"
+            name="phoneNumber"
+            values={values}
+            handleChange={handleChange}
+            handleBlur={handleBlur}
+            errors={errors}
+          />
+
+          <FormTextField
+            className={classes.textField}
             label="Наем"
             name="price"
             values={values}
@@ -288,6 +403,35 @@ const Form = ({
               )
             }}
           />
+        </section>
+        <section className={classes.sectionContainer}>
+          <FormSelectMenu
+            className={classes.selectContainer}
+            name="doesHavePhone"
+            label="Телефон"
+            values={values}
+            errors={errors}
+            setFieldValue={setFieldValue}
+            setFieldTouched={setFieldTouched}
+          >
+            <MenuItem value={"ДА"}>ДА</MenuItem>
+            <MenuItem value={"НЕ"}>НЕ</MenuItem>
+          </FormSelectMenu>
+
+          <FormSelectMenu
+            className={classes.selectContainer}
+            name="tec"
+            label="ТЕЦ"
+            values={values}
+            errors={errors}
+            setFieldValue={setFieldValue}
+            setFieldTouched={setFieldTouched}
+          >
+            <MenuItem value="ДА">ДА</MenuItem>
+            <MenuItem value="НЕ">НЕ</MenuItem>
+            <MenuItem value="Лок.отопл">Лок.отопл.</MenuItem>
+            <MenuItem value="Прокарва се">Прокарва се</MenuItem>
+          </FormSelectMenu>
 
           <FormTextField
             className={classes.textField}
@@ -355,6 +499,15 @@ const Form = ({
           </div>
         </FormControl>
 
+        <div>
+          <ImageGridList
+            values={values}
+            imagesLength={18}
+            onDrop={onDrop}
+            tileData={values.images}
+          />
+        </div>
+
         <Button
           className={classes.submitButton}
           name="newRealEstate"
@@ -382,14 +535,37 @@ const MyEnhancedForm = withFormik({
     propertySellType: "",
     constructionType: "",
     neighborhood: "",
-    city: "",
+    city: "София",
     address: "",
-    price: 0,
-    size: 0,
-    floor: 0,
-    floors: 0,
+    price: "",
+    size: "",
+    floor: "",
+    floors: "",
     description: "",
-    checkboxes: {}
+    doesHavePhone: "",
+    tec: "",
+    phoneNumber: "",
+    checkboxes: {},
+    images: {
+      // image1: "",
+      // image2: "",
+      // image3: "",
+      // image4: "",
+      // image5: "",
+      // image6: "",
+      // image7: "",
+      // image8: "",
+      // image9: "",
+      // image10: "",
+      // image11: "",
+      // image12: "",
+      // image13: "",
+      // image14: "",
+      // image15: "",
+      // image16: "",
+      // image17: "",
+      // image18: ""
+    }
   }),
 
   // Custom sync validation
@@ -398,12 +574,86 @@ const MyEnhancedForm = withFormik({
   handleSubmit: (values, { setSubmitting, errors }) => {
     console.log(values);
     // setSubmitting(false);
-    console.log("TCL: errors", errors);
-    // if (!errors) {
-    // } else {
-    //   console.log("Errors", errors);
-    //   console.log(values);
-    // }
+    const formatTitle = (sellType, city, neighborhood, constructionType) => {
+      // "Дава под Наем 2-СТАЕН, град София, Докторски паметник",
+      if (sellType === "rent") {
+        return `Дава под Наем ${constructionType}, град ${city}, ${neighborhood}`;
+      } else {
+        return `Продава ${constructionType}, град ${city}, ${neighborhood}`;
+      }
+    };
+
+    const getPricePerSquare = (price, size) => {
+      const intPrice = parseInt(price);
+      const intSize = parseInt(size);
+
+      return parseFloat(intPrice / intSize).toFixed(2);
+    };
+
+    const formatFloorString = (floor, floors) => {
+      const getBulgarianNumeral = floor => {
+        switch (floor) {
+          case 1:
+            return "-ви";
+          case 2:
+            return "-ри";
+          default:
+            return "-ти";
+        }
+      };
+
+      const numeral = getBulgarianNumeral(floor);
+
+      return `floor${numeral} от ${floors}`;
+    };
+
+    const getImageNames = values => {
+      return "names";
+    };
+
+    const formatFeaturesForDatabase = checkboxes => {
+      return Object.values(checkboxes)
+        .filter(value => features.includes(value))
+        .join(",");
+    };
+
+    const postObject = {
+      real_estates_id: "2c156544901411455aaaa",
+      real_estates_sell_type: values.propertySellType,
+      real_estates_construction_type: values.constructionType,
+      real_estates_title: formatTitle(
+        values.propertySellType,
+        values.city,
+        values.neighborhood,
+        values.constructionType
+      ),
+      real_estates_neighborhood: values.neighborhood,
+      real_estates_city: values.city,
+      real_estates_address: `град ${values.city}, ${values.neighborhood}, ${values.address}`,
+      real_estates_original_price: parseInt(values.price),
+      real_estates_price_in_euro: parseInt(values.price),
+      real_estates_currency: "EUR",
+      real_estates_price_per_square: `(${getPricePerSquare(
+        values.price,
+        values.size
+      )} EUR/кв.м)`,
+      real_estates_price_per_square_in_euro: parseFloat(
+        getPricePerSquare(values.price, values.size)
+      ),
+      real_estates_size: values.size,
+      real_estates_floor: formatFloorString(values.floor, values.floors),
+      real_estates_tec: values.tec,
+      real_estates_phone: values.doesHavePhone,
+      real_estates_description: values.description,
+      real_estates_imageNames: getImageNames(values),
+      real_estates_seller_phone_number: values.phoneNumber,
+      real_estates_seller_features: formatFeaturesForDatabase(
+        values.checkboxes
+      ),
+      real_estates_created_by: "Profile",
+      real_estates_website_source: "custom"
+    };
+    // axios.post()
   },
 
   displayName: "BasicForm"
