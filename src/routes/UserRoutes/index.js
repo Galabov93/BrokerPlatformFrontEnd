05a8d398836348a30addeaf5f4230332066deaaf
Yordan Galabov <galabovj@gmail.com>
2019-12-14 21:12:18 +0200

    create responsive drawer for mobile

--- src/routes/UserRoutes/index.js
+++ src/routes/UserRoutes/index.js
@@ -17,7 +17,11 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { logout } from "../../services/Authentication";
+import { useTheme } from "@material-ui/styles";
 
 const drawerWidth = 240;
 
@@ -25,84 +29,135 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
   },
-  appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth
-  },
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
   },
-  drawerPaper: {
-    width: drawerWidth
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
   link: {
     textDecoration: "none",
     color: "inherit"
   },
-  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
   content: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3)
+    width: "100%"
+    // padding: theme.spacing(3)
   }
 }));
 
-function UserRoutes({ history }) {
+function UserRoutes({ history, container }) {
   const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   function handleLogout() {
     logout();
     history.push("/login");
   }
 
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {[
+          { text: "Имоти", route: "/real-estates" },
+          { text: "Добави имот", route: "/property/add/100" }
+        ].map((item, index) => (
+          <Link key={item.text} className={classes.link} to={item.route}>
+            <ListItem button>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["Изход"].map((text, index) => (
+          <ListItem button onClick={handleLogout} key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" noWrap>
             REFER+
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {[
-            { text: "Имоти", route: "/real-estates" },
-            { text: "Добави имот", route: "/property/add/100" }
-          ].map((item, index) => (
-            <Link className={classes.link} to={item.route}>
-              <ListItem button key={item.text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={item.text} />
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["Изход"].map((text, index) => (
-            <ListItem button onClick={handleLogout} key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <ProtectedRoute exact path="/" component={() => <div>Dashboard</div>} />
@@ -137,37 +192,3 @@ function UserRoutes({ history }) {
 }
 
 export default withRouter(UserRoutes);
-
-// export default function UserRoutes() {
-//   return (
-//     <div>
-//       {/* <ProtectedRoute component={() => <Header />} /> */}
-//       <ProtectedRoute exact path="/" component={() => <div>Dashboard</div>} />
-//       <ProtectedRoute
-//         exact
-//         path="/real-estates"
-//         component={() => <RealEstates />}
-//       />
-//       <ProtectedRoute
-//         exact
-//         path="/property-page/:id"
-//         component={() => <PropertyPage />}
-//       />
-//       <ProtectedRoute
-//         exact
-//         path="/profile"
-//         component={() => <div>My profile</div>}
-//       />
-//       <ProtectedRoute
-//         exact
-//         path="/property/add/:id"
-//         component={() => <AddNewProperty />}
-//       />
-//       <ProtectedRoute
-//         exact
-//         path="/property/edit/:id"
-//         component={() => <div>Edit a property</div>}
-//       />
-//     </div>
-//   );
-// }
