18b4524c3635715326dbd4ef43efeb96c479445a
Yordan Galabov <galabovj@gmail.com>
2019-12-14 17:12:39 +0200

    config production base url

--- src/config/API.js
+++ src/config/API.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3030"
+    : "http://ec2-18-196-128-38.eu-central-1.compute.amazonaws.com:3030/";
+
 export default axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL,
   responseType: "json"
 });
