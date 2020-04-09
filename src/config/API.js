5b7b1d2235b86a48756c7338643608b1b7014952
Yordan Galabov <galabovj@gmail.com>
2020-04-10 00:35:41 +0300

    add new api url

--- src/config/API.js
+++ src/config/API.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8080"
-    : "http://ec2-18-196-128-38.eu-central-1.compute.amazonaws.com:3030/";
+    ? "localhost:3030/"
+    : "http://ec2-3-127-248-27.eu-central-1.compute.amazonaws.com:3030";
 
 export default axios.create({
   baseURL,
-  responseType: "json"
+  responseType: "json",
 });
